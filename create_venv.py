6b8024c233dc4dbc8b82d0a229e722ba233b99bd
Mieszko Połonczyk <127117681+M-Polonczyk@users.noreply.github.com>
2024-03-20 22:05:35 +0100

    change dirs structure

--- create_venv.py
+++ create_venv.py
@@ -4,15 +4,21 @@ import os
 import subprocess
 
 logging.basicConfig(level=logging.INFO)
+python_projects_path = "python_projects"
 
 # Get the current script directory
 script_directory = os.path.dirname(os.path.realpath(__file__))
+proj_path = os.path.join(script_directory, "python_projects_path")
+
+logging.info("Creating projects directory with virtual environment.")
+os.mkdir(proj_path)
 
 # Set the path for the virtual environment
-venv_path = os.path.join(script_directory, "python_venv")
+venv_path = os.path.join(proj_path, ".venv")
 
 # Check if requirements.txt file exists
 requirements_file = os.path.join(script_directory, "requirements.txt")
+
 
 logging.info("creating virtual environment...")
 # Create virtual environment
@@ -21,9 +27,6 @@ subprocess.run(["python", "-m", "venv", venv_path], check=True)
 if os.path.exists(requirements_file):
     logging.info('run "pip install -r requirements.txt" to install required packages.')
 
-logging.info("Creating projects directory in virtual environment.")
-os.mkdir(os.path.join(venv_path, "projects"))
-
 logging.info(
-    'Virtual environment created and dependencies installed. Store .py files in "projects" directory.'
+    f'Virtual environment created and dependencies installed. Store .py files in "{python_projects_path}" directory.'
 )
